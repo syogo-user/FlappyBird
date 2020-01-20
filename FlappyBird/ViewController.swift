ad96f02685a4f01ea0514b77623f7ee3e02dd551
syogo-user <s.photo.trumpet.005@gmail.com>
2020-01-20 16:50:18 +0900

    FlappyBird 8.2まで完了

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -6,15 +6,36 @@
 //  Copyright © 2020 syogo-user. All rights reserved.
 //
 
-import UIKit
+
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //SKViewの型を変換する
+        let skView = self.view as! SKView
+        
+        //FPSを表示する
+        skView.showsFPS = true
+        
+        //ノードの数を表示する
+        skView.showsNodeCount = true
+        
+        //ビューと同じサイズでシーンを作成する
+        let scene = GameScene(size:skView.frame.size)
+        
+        //ビューにシーンを表示する
+        skView.presentScene(scene)
     }
-
+    
+    //ステータスバーを消す
+    override var prefersStatusBarHidden: Bool{
+        get {
+            return true
+        }
+    }
 
 }
 
